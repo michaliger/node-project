51747183a0b5b419c64eabba4263d2a8f02d2bd7
michaliger <michal6761991@gmail.com>
2025-11-17 01:05:37 +0200

    update series.routes.js

--- routes/series.routes.js
+++ routes/series.routes.js
@@ -8,7 +8,12 @@ const {
   deleteseries
 } = require('../controllers/series.controller');
 
+const { protect, restrictTo } = require('../middleware/auth');
+
 const router = express.Router();
+
+// הגנה על כל הראוטים (אם את רוצה – אפשר להסיר)
+router.use(protect);
 
 // GET /api/series
 // POST /api/series

7580206b5d6588191ce46df6edb1e55cbe580435
michaliger <michal6761991@gmail.com>
2026-07-04 23:43:25 +0300

    add adress render

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // פתרון CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176','https://torahfiles.onrender.com'],
   credentials: true
 }));
 
